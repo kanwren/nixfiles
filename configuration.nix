7fd6fc486bd04c445cb5f394b8154716800e5140
Nicole Prindle <nprindle18@gmail.com>
2019-11-10 19:26:55 -0500

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,140 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking =
+    let
+      secrets = import ./secrets.nix;
+    in
+    {
+      hostName = "nprin";
+
+      networkmanager = {
+        enable = true;
+      };
+
+      wireless = {
+        enable = false;
+
+        networks = {
+          eduroam = {
+            auth = secrets.auth.eduroam;
+          };
+        };
+      };
+
+      useDHCP = false;
+      interfaces.eno1.useDHCP = true;
+      interfaces.wlo1.useDHCP = true;
+
+      firewall = {
+        enable = true;
+        allowedTCPPorts = [ ];
+        allowedUDPPorts = [ ];
+      };
+    };
+
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time = {
+    timeZone = "America/New_York";
+    hardwareClockInLocalTime = false;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment = {
+    systemPackages = with pkgs; [
+      wget
+      curl
+      vim_configurable
+      bash
+      git
+      tmux
+      alacritty
+      firefox
+      tldr
+      xclip
+    ];
+
+    interactiveShellInit = ''
+      set -o vi
+    '';
+
+    # nixpkgs.config.vim = {
+    #   ftNixSupport = true;
+    # };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  services.xserver = {
+    enable = true;
+    libinput.enable = true;
+    layout = "us";
+    xkbOptions = "caps:escape";
+    desktopManager = {
+      default = "none";
+      xterm.enable = false;
+    };
+
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      extraPackages = with pkgs; [
+        dmenu
+        i3status
+        i3lock
+      ];
+    };
+  };
+
+  users.users = {
+    nprin = {
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+    };
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+
+}
+
