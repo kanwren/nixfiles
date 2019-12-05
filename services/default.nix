5c27c8ff0ccd10d4cab6d3c7afbbff291bceeadf
Nicole Prindle <nprindle18@gmail.com>
2019-12-04 21:43:25 -0500

    Add compton configuration

--- services/default.nix
+++ services/default.nix
@@ -31,5 +31,21 @@
         ];
       };
     };
+
+    compton = {
+      enable = true;
+
+      # activeOpacity = "0.85";
+      # inactiveOpacity = "0.9";
+      opacityRules = [
+        "85:class_g = 'Alacritty'"
+      ];
+
+      shadow = true;
+
+      # 4ms fade when changing opacity
+      fade = true;
+      fadeDelta = 4;
+    };
   };
 }
