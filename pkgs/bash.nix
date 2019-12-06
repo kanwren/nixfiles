04964ff89372e2328ca097fb556ac04f453c6446
Nicole Prindle <nprindle18@gmail.com>
2019-12-06 02:14:09 -0500

    Add more handly bash aliases

--- pkgs/bash.nix
+++ pkgs/bash.nix
@@ -17,6 +17,7 @@ rec {
       egrep = "${gnugrep}/bin/egrep -E --color=auto";
       diff = "${colordiff}/bin/colordiff";
 
+      ".." = "cd ..";
       ".1" = "cd ..";
       ".2" = "cd ../..";
       ".3" = "cd ../../..";
@@ -30,8 +31,9 @@ rec {
       cleantex = "rm *.aux *.log";
       copylast = "fc -ln -1 | ${gawk}/bin/awk '{\$1=\$1}1' | ${xclip}/bin/xclip -sel clip";
       xc = "${xclip}/bin/xclip -sel clip";
+      getpass = "${openssl}/bin/openssl rand -base64";
 
-      nrn = "${nix}/bin/nix repl '<nixpkgs>'";
+      nrn = "${nix}/bin/nix repl '<nixpkgs>' '<nixpkgs/nixos>'";
     };
 
     interactiveShellInit = ''
