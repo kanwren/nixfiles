46e2d4241d8986297b4d314ff0ff53b31097000f
Nicole Prindle <nprindle18@gmail.com>
2019-12-07 02:36:59 -0500

    Add overlay to pull from NUR

---
+++ nix/overlays/nur.nix
@@ -0,0 +1,12 @@
+self: super:
+
+{
+  nur =
+    let nurSrc = super.fetchFromGitHub {
+      owner = "nix-community";
+      repo = "NUR";
+      rev = "e3d491d7ec3af4afef742650a1232f8318b01773";
+      sha256 = "1rbz2jprkfgshfa8kyfd2k79rhwfgc9a6nm3w41m2gs854s9v7mw";
+    };
+    in import nurSrc { pkgs = self; };
+}
