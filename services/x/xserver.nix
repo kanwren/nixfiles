134a798fd83a1d36c6c0d3004efe5d3296a8a2dd
Nicole Prindle <nprindle18@gmail.com>
2019-12-08 13:49:54 -0500

    Remove duplicate xserver configs

--- services/x/xserver.nix
+++ services/x/xserver.nix
@@ -5,6 +5,7 @@
 
     enable = true;
 
+    # Enable touchpad
     libinput.enable = true;
 
     layout = "us";
@@ -19,16 +20,21 @@
       enable = true;
       package = pkgs.i3-gaps;
       configFile = ./i3/config;
+
+      # Make sure the desktop background is loaded, if it exists
       extraSessionCommands =
         let bg = ./i3/desktop_bg.png;
         in pkgs.lib.optionalString (builtins.pathExists bg) ''
           ${pkgs.feh}/bin/feh --bg-fill ${builtins.toString bg}
         '';
+
       extraPackages = with pkgs; [
         dmenu
         i3status
         i3lock
+        # Applets
         networkmanagerapplet
+        blueman
       ];
     };
 
