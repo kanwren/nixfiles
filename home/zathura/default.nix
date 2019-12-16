5b87413e3d61c60fe9a17ea6a1b510c6a7b1ac2b
Nicole Prindle <nprindle18@gmail.com>
2019-12-16 17:06:26 -0500

    Use built-in home-manager zathura configurations

--- home/zathura/default.nix
+++ home/zathura/default.nix
@@ -3,13 +3,14 @@
 {
   home-manager.users.nprin = {
 
-    home.packages = [ pkgs.zathura ];
-
-    home.file = {
-      ".config/zathura/.zathurarc".text = ''
-        set statusbar-h-padding 0
-        set statusbar-v-padding 0
-        set page-padding 1
+    programs.zathura = {
+      enable = true;
+      options = {
+        statusbar-h-padding = 0;
+        statusbar-v-padding = 0;
+        page-padding = 1;
+      };
+      extraConfig = ''
         map i recolor
         map p print
       '';
