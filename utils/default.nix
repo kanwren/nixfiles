b8061315e358e88d6cf036fba4f702053b27dcf1
Nicole Prindle <nprindle18@gmail.com>
2019-12-31 15:14:49 -0500

    Add getFilesWith for finding files matching a predicate

--- utils/default.nix
+++ utils/default.nix
@@ -35,12 +35,15 @@
         (builtins.readDir dir)));
 
       # Get all files in a directory
-      getFiles = dir:
+      getFiles = getFilesWith (_: _: true);
+
+      # Get all files in a directory matching a predicate
+      getFilesWith = pred: dir:
         builtins.map (x: dir + "/${x}")
         (builtins.attrNames
-        (lib.filterAttrs (_: type: type == "regular" || type == "symlink")
+        (lib.filterAttrs (name: type:
+          pred name type && (type == "regular" || type == "symlink"))
         (builtins.readDir dir)));
-
     };
   };
 }
