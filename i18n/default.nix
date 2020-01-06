e6ad91c47d3b46928471a18599234e663a689c6e
Nicole Prindle <nprindle18@gmail.com>
2020-01-05 22:25:40 -0500

    Add IBus and pinyin input

--- i18n/default.nix
+++ i18n/default.nix
@@ -1,9 +1,16 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "us";
     defaultLocale = "en_US.UTF-8";
+
+    inputMethod = {
+      enabled = "ibus";
+      ibus = {
+        engines = with pkgs.ibus-engines; [ libpinyin ];
+      };
+    };
   };
 }
