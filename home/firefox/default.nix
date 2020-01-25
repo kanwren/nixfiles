0ea0fafdea9d186a3edfbcc53ffed0a1f7054dd3
Nicole Prindle <nprindle18@gmail.com>
2020-01-24 23:24:11 -0500

    Remove some Firefox configuration, add octotree

--- home/firefox/default.nix
+++ home/firefox/default.nix
@@ -9,6 +9,7 @@
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         vim-vixen
         darkreader
+        octotree
       ];
 
       profiles = {
@@ -16,12 +17,6 @@
           name = "nprin";
           id = 0;
           isDefault = true;
-          settings = {
-            "browser.contentblocking.category" = "standard";
-            "browser.tabs.warnOnClose" = false;
-            "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
-          };
-          userChrome = builtins.readFile ./userChrome.css;
         };
       };
 
