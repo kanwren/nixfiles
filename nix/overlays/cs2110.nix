33752dc6acff6d52ee76d7912626507e6d6c4995
Nicole Prindle <nprindle18@gmail.com>
2020-01-29 15:10:51 -0500

    Bump cs2110 overlay version to patch security hole

--- nix/overlays/cs2110.nix
+++ nix/overlays/cs2110.nix
@@ -5,8 +5,8 @@ let
   cs2110src = super.fetchFromGitHub {
     owner = "nprindle";
     repo = "cs2110-nix";
-    rev = "47cc03fae7b955fc374717f2cef611016819a158";
-    sha256 = "013fwkd9hwpm7wlyld4hsmr826cklwrhgd3m7hjrxq9qpc80zp3k";
+    rev = "dd1912f9882b9c46c2154f5582afeaee3f6ec92a";
+    sha256 = "1fv7anlz2hz5amsryjmvfk4kmgyj4zbvlbric803gmnnp54l0mdw";
   };
 in {
   cs2110 = import cs2110src {};
