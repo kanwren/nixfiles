c046143931c89aa5503a855c059550395d653a23
Nicole Prindle <nprindle18@gmail.com>
2020-02-10 01:55:24 -0500

    Add githug to user packages

--- home/default.nix
+++ home/default.nix
@@ -22,6 +22,13 @@ with rec {
     rev = "5969f2ff96dbfe3212f342ca0818ea2908a631ac";
     sha256 = "1lqvkfsz96df8i7628mjkm1az9rbmb5mibb2xlgxaf5px7lmr2qf";
   }) {};
+  githug = pkgs.buildRubyGem {
+    inherit (pkgs) ruby;
+    pname = "githug";
+    version = "0.5.0";
+    gemName = "githug";
+    source.sha256 = "0hk4pvbvjxipapzjr9rrhcvm2mxlw4a8f6bsfqgq1wnvlbmmrzc6";
+  };
 };
 
 {
@@ -80,6 +87,7 @@ with rec {
 
         # Games
         infinisweep
+        githug
 
         # Applications
         discord
