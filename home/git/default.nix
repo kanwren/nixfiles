ebde271d0c1a13e755fa097c2b5555f6dcd1bc68
Nicole Prindle <nprindle18@gmail.com>
2020-02-16 01:47:20 -0500

    Add more useful git aliases

--- home/git/default.nix
+++ home/git/default.nix
@@ -15,19 +15,24 @@
       ];
 
       aliases = {
-        # Abbreviations for filling in options
+        # Aliases for filling in options
         cane = "commit --amend --no-edit";
         changed = "show --name-status --oneline";
+        root = "rev-parse --show-toplevel";
+        diffc = "diff --cached";
+        mkexec = "update-index --chmod=+x";
+        rh = "reset --hard";
+        ri = "rebase --interactive";
+        # `git log` aliases
         cmp = "log --left-right --graph --oneline";
         graph = "log --graph --oneline";
         ls = "log --oneline";
-        rh = "reset --hard";
-        root = "rev-parse --show-toplevel";
-        diffc = "diff --cached";
-        exec = "update-index --chmod=+x";
         # Abbreviations for common commands
         s = "status";
         co = "checkout";
+        # Utility
+        alias = ''"! f(){ git config --get-regexp ^alias | cut -c 7- | sed -e \"s/ \\(.*\\)/ = \\1/\"; }; f"'';
+        ignore-io = ''"! f(){ join(){ local IFS=\",\"; echo \"$*\"; }; curl https://www.gitignore.io/api/$(join $*); }; f"'';
         # Jokes
         praise = "blame";
         pansect = "bisect";
