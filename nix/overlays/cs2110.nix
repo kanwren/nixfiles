35c26e31587c31702df48f7a2e572218089a9db2
Nicole Prindle <nprindle18@gmail.com>
2020-03-23 18:50:23 -0400

    Bump cs2110-nix rev

--- nix/overlays/cs2110.nix
+++ nix/overlays/cs2110.nix
@@ -5,8 +5,8 @@ let
   cs2110src = super.fetchFromGitHub {
     owner = "nprindle";
     repo = "cs2110-nix";
-    rev = "93024df307d7af37d26965a0047bc18700b9f912";
-    sha256 = "0ngv7zbg1jc5rmciwb56lks0kwbq1g1cwa3wl2y1vgchnfbbh8ia";
+    rev = "1cb125c77884acd8103810f5640027079f9a15d4";
+    sha256 = "0ymd9lbzr2qfvgkdvw95gwk64bzjqrf7li9k1wdrs9qnpvyy5bmq";
   };
 in {
   cs2110 = import cs2110src {};
