27acfececcce4c4e5437b61a8e08bd18aaea1369
Nicole Prindle <nprindle18@gmail.com>
2020-04-30 15:21:25 -0400

    Remove HIE and hasktags

--- home/haskell/default.nix
+++ home/haskell/default.nix
@@ -6,8 +6,6 @@
     home.packages = with pkgs; [
       cabal-install
       ghcid
-      haskellPackages.hasktags
-      (all-hies.selection { selector = p: { inherit (p) ghc865 ghc881; }; })
     ];
 
   };
