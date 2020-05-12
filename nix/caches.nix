0d6a1f6f8293876b2cc319cb2781ed76ac2e9c5a
Nicole Prindle <nprindle18@gmail.com>
2020-05-12 16:00:52 -0400

    Use IOHK and clever's binary caches

--- nix/caches.nix
+++ nix/caches.nix
@@ -4,12 +4,15 @@
   nix = {
     # cache.nixos.org is included by default
     binaryCaches = [
-      # personal cache
       "https://nprindle.cachix.org"
+      "http://cache.earthtools.ca"
+      "https://hydra.iohk.io"
     ];
 
     binaryCachePublicKeys = [
       "nprindle.cachix.org-1:hRW0f/n4hCZZzTzYJO9olDjJ+8MB4VpknEGpiVCxpWo="
+      "c2d.localnet-1:YTVKcy9ZO3tqPNxRqeYEYxSpUH5C8ykZ9ImUKuugf4c="
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
     ];
   };
 }
