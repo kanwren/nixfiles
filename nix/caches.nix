f23a485f425ba3aecd879a732548d0eb099068bd
Nicole Prindle <nprindle18@gmail.com>
2020-05-12 15:57:57 -0400

    Remove hie cache

--- nix/caches.nix
+++ nix/caches.nix
@@ -6,13 +6,10 @@
     binaryCaches = [
       # personal cache
       "https://nprindle.cachix.org"
-      # haskell-ide-engine cache
-      "https://all-hies.cachix.org"
     ];
 
     binaryCachePublicKeys = [
       "nprindle.cachix.org-1:hRW0f/n4hCZZzTzYJO9olDjJ+8MB4VpknEGpiVCxpWo="
-      "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
     ];
   };
 }
