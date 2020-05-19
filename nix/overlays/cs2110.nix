cfd3d9321174cd3c067c14fb379ae04f8ced97ea
Nicole Prindle <nprindle18@gmail.com>
2020-05-19 19:34:18 -0400

    Bump cs2110 rev for new CircuitSim

--- nix/overlays/cs2110.nix
+++ nix/overlays/cs2110.nix
@@ -5,8 +5,8 @@ let
   cs2110src = super.fetchFromGitHub {
     owner = "nprindle";
     repo = "cs2110-nix";
-    rev = "1cb125c77884acd8103810f5640027079f9a15d4";
-    sha256 = "0ymd9lbzr2qfvgkdvw95gwk64bzjqrf7li9k1wdrs9qnpvyy5bmq";
+    rev = "bf55d706c8f554d4c9552e87d562f6520c448112";
+    sha256 = "1i7jkdw09v88b0jmmmhyr0vsdw1fznr7l18xwh23kswbiin2czxx";
   };
 in {
   cs2110 = import cs2110src {};
