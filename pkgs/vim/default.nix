791df9d82fda1b04977a1dc7006f11aa14b53fca
Nicole Prindle <nprindle18@gmail.com>
2020-07-19 21:05:53 -0400

    Switch to neovim

--- pkgs/vim/default.nix
+++ pkgs/vim/default.nix
@@ -1,28 +1,17 @@
 { pkgs, ... }:
 
-let
-  myVim = with pkgs; vim_configurable.override {
-    python = python3;
-  };
-in
 {
   environment = {
     systemPackages = [
-      myVim
-      pkgs.rnix-lsp # nix lsp via rnix
+      (pkgs.neovim.override {
+        viAlias = true;
+        vimAlias = true;
+      })
     ];
-    shellAliases.vi = "vim";
     variables = {
-      # Make vim the default editor
-      EDITOR = "vim";
-      VISUAL = "vim";
-    };
-  };
-
-  programs = {
-    bash.shellAliases = {
-      vi = "vim";
-      svim = "sudoedit";
+      # Make neovim the default editor
+      EDITOR = "nvim";
+      VISUAL = "nvim";
     };
   };
 }
