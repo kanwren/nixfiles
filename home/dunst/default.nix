204921af6be686b033e220a8ee30162e8bffbdba
Nicole Prindle <nprindle18@gmail.com>
2020-07-26 19:19:51 -0400

    Configure dunst

--- home/dunst/default.nix
+++ home/dunst/default.nix
@@ -12,30 +12,34 @@
     settings = {
       global = {
         geometry = "500x5-10+10";
-        transparency = 10;
-        frame_color = "#000000";
-        font = "Fira Mono";
-        corner_radius = 3;
-        word_wrap = true;
-        follow = "mouse";
-        notification_height = 20;
         padding = 5;
         horizontal_padding = 5;
+        notification_height = 20;
+        corner_radius = 3;
         icon_position = "right";
+        max_icon_size = 75;
+        transparency = 20;
+        frame_color = "#88c0d0";
+        frame_width = 2;
+        word_wrap = true;
+        follow = "mouse";
+        font = "Fira Mono";
+        markup = "full";
+        format = "%s\n%b";
       };
       urgency_low = {
-        background = "#37474f";
-        foreground = "#eceff1";
+        background = "#000000";
+        foreground = "#ffffff";
         timeout = 10;
       };
       urgency_normal = {
-        background = "#37474f";
-        foreground = "#eceff1";
+        background = "#000000";
+        foreground = "#ffffff";
         timeout = 10;
       };
       urgency_critical = {
-        background = "#37474f";
-        foreground = "#eceff1";
+        background = "#000000";
+        foreground = "#ffffff";
         timeout = 10;
       };
     };
