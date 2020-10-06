e3e78853f2e32365b53e355cf7b1f18b697a9a77
Nicole Prindle <nprindle18@gmail.com>
2020-10-05 23:00:32 -0400

    Bump HLS

--- nix/overlays/hls.nix
+++ nix/overlays/hls.nix
@@ -1,7 +1,7 @@
 self: super:
 
 let
-  hlsVersion = "0.4.0";
+  hlsVersion = "0.5.0";
 
   hls-ghc-executable = { ghcVersion, sha256 }: {
     execName = "haskell-language-server-${ghcVersion}";
@@ -12,16 +12,16 @@ let
   hls-wrapper = {
     execName = "haskell-language-server-wrapper";
     url = "https://github.com/haskell/haskell-language-server/releases/download/${hlsVersion}/haskell-language-server-wrapper-Linux.gz";
-    sha256 = "026hmd6c8ilf7p5h3rk63ywd358hm0xbmniplnkdfilgri3j26sm";
+    sha256 = "1m1zn3lar2lgsr29vxbcay58mpny1gzzf1mfmpyda6rr61gy5z2a";
   };
   hls-executables = [ hls-wrapper ] ++ builtins.map hls-ghc-executable [
-    { ghcVersion = "8.6.4"; sha256 = "0fhzm9190mgya08mcjz9zrd34p1s3k1a1l9dzxpxm7ij9b43n333"; }
-    { ghcVersion = "8.6.5"; sha256 = "0d4lwfy3ywrmz5qppzq11khk9n9744zrmgp5nl618dcl5di1w0aa"; }
-    { ghcVersion = "8.8.2"; sha256 = "1q68gvz9pjzfwsssa6w2qqxx7x6lxqnlvn8wm89g2z64ga5ayay2"; }
-    { ghcVersion = "8.8.3"; sha256 = "1ii7ad3kbl27nfyrvwqb5ban09vdap2y56gyd8gbs3g6zjrphx81"; }
-    { ghcVersion = "8.8.4"; sha256 = "0df1l8clrch19dsz8dxr2y7qzdm6c71jwik28l5sl5p1qzngiv47"; }
-    { ghcVersion = "8.10.1"; sha256 = "0ldsxq02mzxhp11690ajqn0yfw3m115057bgxza9336ahdfxni7m"; }
-    { ghcVersion = "8.10.2"; sha256 = "14dzs78s8rns04rhw0g87c4jkvhnfindz8gkj7aw2ygd4hw9ayyk"; }
+    { ghcVersion = "8.6.4"; sha256 = "1n24lpq230r3q6661lllmk4jm5fi9z47g15p2dzm3vznahsy1qxh"; }
+    { ghcVersion = "8.6.5"; sha256 = "0bd9d7pm61l9lr81jbw6x5crb2v0pdl1pzj3fand8sibz2sxnnab"; }
+    { ghcVersion = "8.8.2"; sha256 = "1w3sfrb0nbfxrjr7a63pf8ddx58nvm3qsgqkswfkklyjyxhw70g6"; }
+    { ghcVersion = "8.8.3"; sha256 = "00qfvay2fr4vmiraxv1f40g7cns2iqlav5sh454pv91z6k95pg1i"; }
+    { ghcVersion = "8.8.4"; sha256 = "167bqvlxvhscbfdj4p29l026n8235b8zq1ycjsjyx5in19kz74an"; }
+    { ghcVersion = "8.10.1"; sha256 = "0868ffnkcq7ls7cnwlxj2hx05yj67lgsc5hkdlvn5am1z49idy5i"; }
+    { ghcVersion = "8.10.2"; sha256 = "09al67kj3yrig682j986pfn4pvvbjddxj2aqj6vgv8d67sjirn4j"; }
   ];
 
   fetchHls = executables:
