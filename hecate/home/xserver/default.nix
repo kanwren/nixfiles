4b583803a18854cc757d13546ef43f663ac4423d
Nicole Prindle <nprindle18@gmail.com>
2021-01-17 01:03:36 -0500

    Add more compose combinations

--- hecate/home/xserver/default.nix
+++ hecate/home/xserver/default.nix
@@ -1,12 +1,65 @@
-{ ... }:
+{ lib, ... }:
 
-{
+let
+  idPairs = xs: lib.listToAttrs (builtins.map (x: { name = x; value = x; }) xs);
+  numbers = idPairs (builtins.map builtins.toString (lib.range 0 9));
+  letters = idPairs (lib.stringToCharacters "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  symbols = {
+    "-" = "minus";
+    " " = "space";
+    "<" = "less";
+    ">" = "greater";
+    "=" = "equal";
+    "_" = "underscore";
+    "(" = "parenleft";
+    ")" = "parenright";
+    "/" = "slash";
+    "\\" = "backslash";
+    "." = "period";
+    "," = "comma";
+    "|" = "bar";
+    "~" = "asciitilde";
+  };
+  common = letters // numbers // symbols;
+  toKeys = str: builtins.map (x: common.${x}) (lib.stringToCharacters str);
+in {
   xserver.xcompose = {
     enable = true;
     includeLocale = true;
     mappings = [
-      { keys = [ "0" "space" ]; result = "​"; }
-      { keys = [ "space" "0" ]; result = "​"; }
+      # Characters
+      { keys = toKeys "0 "; result = "​"; }
+      { keys = toKeys " 0"; result = "​"; }
+      { keys = toKeys "~-"; result = "～"; }
+      { keys = toKeys "-~"; result = "～"; }
+      # Math
+      { keys = toKeys "<>";      result = "⋄"; }
+      { keys = toKeys "<_>";     result = "↔"; }
+      { keys = toKeys "==";      result = "⇔"; }
+      { keys = toKeys "/_=";     result = "≢"; }
+      { keys = toKeys "(-";      result = "∈"; }
+      { keys = toKeys "/(-";     result = "∉"; }
+      { keys = toKeys ")-";      result = "∋"; }
+      { keys = toKeys "/)-";     result = "∌"; }
+      { keys = toKeys "(C";      result = "⊂"; }
+      { keys = toKeys "/(C";     result = "⊄"; }
+      { keys = toKeys ")C";      result = "⊃"; }
+      { keys = toKeys "/)C";     result = "⊅"; }
+      { keys = toKeys "(_";      result = "⊆"; }
+      { keys = toKeys "/(_";     result = "⊆"; }
+      { keys = toKeys ")_";      result = "⊇"; }
+      { keys = toKeys "/)_";     result = "⊇"; }
+      { keys = toKeys ")U";      result = "⋂"; }
+      { keys = toKeys "(U";      result = "⋃"; }
+      { keys = toKeys "forall";  result = "∀"; }
+      { keys = toKeys "exists";  result = "∃"; }
+      { keys = toKeys "/exists"; result = "∄"; }
+      { keys = toKeys "_|_";     result = "⊥"; }
+      { keys = toKeys "|-";      result = "⊢"; }
+      { keys = toKeys "()";      result = "∘"; }
+      # Emoticons
+      { keys = toKeys "SHRUG"; result = ''¯\_(ツ)_/¯''; }
+      { keys = toKeys "SQUID"; result = ''くコ:彡''; }
     ];
   };
 }
