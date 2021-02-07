60fb888507322b42725be1f98e9c1f9a629a11b3
Nicole Prindle <nprindle18@gmail.com>
2021-02-07 16:12:10 -0500

    Add plugin configs to kak

--- hecate/home/kakoune/default.nix
+++ hecate/home/kakoune/default.nix
@@ -1,12 +1,27 @@
 { pkgs, ... }:
 
-{
+let
+  kak-smartindent = pkgs.stdenv.mkDerivation {
+    name = "kak-smartindent";
+    src = pkgs.fetchFromGitHub {
+      owner = "andreyorst";
+      repo = "smarttab.kak";
+      rev = "6e3264df7a9a786961e634d5ae8fb117ca86ef0a";
+      sha256 = "00r9i4v6aiy6yabf22y27idl2cvjivdfwn21aypxr0d849rfg6q8";
+    };
+    installPhase = ''
+      mkdir -p "$out"/share/kak/autoload/plugins/smartindent
+      cp -r rc/*.kak "$out"/share/kak/autoload/plugins/smartindent
+    '';
+  };
+in {
   programs.kakoune = {
     enable = true;
     extraConfig = builtins.readFile ./kakrc;
-    plugins = with pkgs.kakounePlugins; [
+    plugins = with pkgs; with pkgs.kakounePlugins; [
       pkgs.kak-lsp
       kak-fzf
+      kak-smartindent
     ];
   };
 }
