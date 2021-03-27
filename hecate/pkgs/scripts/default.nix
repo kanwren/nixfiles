0660ba995fc9454af10eaad481449455cef1ba06
Nicole Prindle <nprindle18@gmail.com>
2021-03-27 02:25:52 -0400

    Add script to append to rpaths

--- hecate/pkgs/scripts/default.nix
+++ hecate/pkgs/scripts/default.nix
@@ -93,6 +93,43 @@ let
       "${pkgs.jq}/bin/jq" -f "${./circuitsim-history.jq}" "$1"
     '';
 
+    # Append a path to the rpath of an executable
+    add-rpath = with pkgs; writeShellScriptBin "add-rpath" ''
+      set -euo pipefail
+
+      print_usage() { >&2 echo "Usage: add-rpath [-a|--append] <path> <files...>"; }
+      if ! args=$(getopt -o ah --long append,help -n csrh -- "$@"); then
+        print_usage; exit 1
+      fi
+      eval set -- "$args"
+
+      appendType="prepend"
+      for opt; do
+        case "$opt" in
+          -h|--help) print_usage; exit 0 ;;
+          -a|--append) appendType="append"; shift; break ;;
+          --) shift; break ;;
+          *) exit 1 ;;
+        esac
+      done
+
+      if [ $# -lt 2 ]; then
+        print_usage; exit 1
+      fi
+
+      path="$1"
+      shift
+      for f in "$@"; do
+        cur_rpath="$(patchelf --print-rpath "$f")"
+        if [ "$appendType" = "prepend" ]; then
+          new_rpath="$path:$cur_rpath"
+        elif [ "$appendType" = "append" ]; then
+          new_rpath="$cur_rpath:$path"
+        fi
+        ${patchelf}/bin/patchelf --set-rpath "$new_rpath" "$f"
+      done
+    '';
+
     # Print nix garbage collector roots that still exist
     gcroots = with pkgs; writeShellScriptBin "nix-gcroots" ''
       echo "/nix/var/nix/gcroots/auto:"
