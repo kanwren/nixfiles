1c4e6b7ee878d3f4d6fc476426a5cd0bb44d7329
Nicole Prindle <nprindle18@gmail.com>
2021-04-08 02:42:26 -0400

    Add code symlink to codium

--- hecate/home/vscode/default.nix
+++ hecate/home/vscode/default.nix
@@ -3,7 +3,11 @@
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    package = pkgs.vscodium.overrideAttrs (old: {
+      installPhase = (old.installPhase or "") + ''
+        ln -s "$out"/bin/codium "$out"/bin/code
+      '';
+    });
     extensions = with pkgs.vscode-extensions; [
       haskell.haskell
     ];
