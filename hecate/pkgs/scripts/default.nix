ac5fc47cd1ff9e66e8b603c520eebacb24db51c6
Nicole Prindle <nprindle18@gmail.com>
2021-04-10 14:43:04 -0400

    Add script to get firefox to open like a webapp

--- hecate/pkgs/scripts/default.nix
+++ hecate/pkgs/scripts/default.nix
@@ -202,6 +202,39 @@ let
       done
     '';
 
+    # Hack to get firefox to open like chrome's --app
+    firefox-app = pkgs.writeShellScriptBin "firefox-app" ''
+      set -euo pipefail
+
+      profile_path="$HOME/.local/share/firefox-profiles/app"
+      chrome_path="$profile_path/chrome"
+
+      if [ ! -d "$profile_path" ]; then
+        echo "Initializing Firefox app mode..."
+        mkdir -p "$profile_path" "$chrome_path"
+
+        echo "Enabling userChrome.css..."
+        cat << EOF >> "$profile_path"/prefs.js
+      user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+      EOF
+
+        echo "Writing custom userChrome.css..."
+        cat << EOF > "$chrome_path"/userChrome.css
+      @namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul");
+      #TabsToolbar { visibility: collapse !important; }
+      #BookmarksToolbar { visibility: collapse !important; }
+      #nav-bar { visibility: collapse !important; }
+      #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+        visibility: collapse !important;
+      }
+      EOF
+
+        echo "Done."
+      fi
+
+      ${pkgs.firefox}/bin/firefox -profile "$profile_path" "$@"
+    '';
+
     toggle = pkgs.writeShellScriptBin "toggle" ''
       if [ $# -ne 1 ]; then
         >&2 echo "Usage: toggle PROGRAM"
