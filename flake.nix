e354dbbd15b70f2bf1201999fa6fe4d66df88fb7
Nicole Prindle <nprindle18@gmail.com>
2021-05-11 01:56:04 -0400

    Name the default bundler

--- flake.nix
+++ flake.nix
@@ -212,27 +212,31 @@
         };
       };
 
-      # nix-bundle shim to bundle programs with names other than the default
-      # See https://github.com/matthewbauer/nix-bundle/issues/74
-      defaultBundler = { program, system }:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          bundle = import nix-bundle { nixpkgs = pkgs; };
-          envProg = builtins.getEnv "PROGRAM";
-          prog =
-            if envProg == "" then
-              builtins.trace "Warning: PROGRAM not set; defaulting to '${program}'. Did you forget to set PROGRAM or --impure?" program
-            else
-              "${builtins.dirOf program}/${envProg}";
-          script = pkgs.writeScript "startup" ''
-            #!/bin/sh
-            .${bundle.nix-user-chroot}/bin/nix-user-chroot -n ./nix -- "${prog}"
-          '';
-        in
-        bundle.makebootstrap {
-          targets = [ script ];
-          startup = ".${builtins.unsafeDiscardStringContext script} '\"$@\"'";
-        };
+      bundlers = {
+        # nix-bundle shim to bundle programs with names other than the default
+        # See https://github.com/matthewbauer/nix-bundle/issues/74
+        nix-bundle = { program, system }:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            bundle = import nix-bundle { nixpkgs = pkgs; };
+            envProg = builtins.getEnv "PROGRAM";
+            prog =
+              if envProg == "" then
+                builtins.trace "Warning: PROGRAM not set; defaulting to '${program}'. Did you forget to set PROGRAM or --impure?" program
+              else
+                "${builtins.dirOf program}/${envProg}";
+            script = pkgs.writeScript "startup" ''
+              #!/bin/sh
+              .${bundle.nix-user-chroot}/bin/nix-user-chroot -n ./nix -- "${prog}"
+            '';
+          in
+          bundle.makebootstrap {
+            targets = [ script ];
+            startup = ".${builtins.unsafeDiscardStringContext script} '\"$@\"'";
+          };
+      };
+
+      defaultBundler = self.bundlers.nix-bundle;
     } // flake-utils.lib.eachDefaultSystem (system: (
       let
         pkgs = nixpkgs.legacyPackages.${system};
