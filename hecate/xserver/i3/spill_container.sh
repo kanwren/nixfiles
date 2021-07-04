4aeb43a6390e6f3d40ccec3b1d1bad751ae72acf
Nicole Prindle <nprindle18@gmail.com>
2021-07-04 00:48:38 -0400

    Fix i3 crash when spilling containers to non-container

--- hecate/xserver/i3/spill_container.sh
+++ hecate/xserver/i3/spill_container.sh
@@ -9,15 +9,19 @@ define query_parent_children <<'EOF'
 .
 | (recurse(.nodes[]) | select(.nodes | any(.focused))) as $parent
 | ($parent.nodes[] | select(.focused)) as $focused
-| { parent: $parent.id, children: ($focused.nodes | map(.id) | reverse) }
+| { parent: $parent.id, focused_type: $focused.type, children: ($focused.nodes | map(.id) | reverse) }
 EOF
 
 layout="$(i3-msg -t get_tree | jq "$query_parent_children")"
 
-i3-msg -t command "[con_id=\"$(echo "$layout" | jq ".parent")\"] mark _i3_spill_dest_con"
-while IFS= read -r id; do
-  if [ -n "$id" ]; then
-    i3-msg -t command "[con_id=\"$id\"] move container to mark _i3_spill_dest_con"
-  fi
-done <<< "$(echo "$layout" | jq -r ".children[]")"
-i3-msg -t command "unmark _i3_spill_dest_con"
+# Trying to move a container to the container holding the workspaces would crash i3
+focused_type="$(echo "$layout" | jq -r ".focused_type")"
+if [ "$focused_type" = "con" ]; then
+  i3-msg -t command "[con_id=\"$(echo "$layout" | jq -r ".parent")\"] mark _i3_spill_dest_con"
+  while IFS= read -r id; do
+    if [ -n "$id" ]; then
+      i3-msg -t command "[con_id=\"$id\"] move container to mark _i3_spill_dest_con"
+    fi
+  done <<< "$(echo "$layout" | jq -r ".children[]")"
+  i3-msg -t command "unmark _i3_spill_dest_con"
+fi
