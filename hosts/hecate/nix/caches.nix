3772b76d286803ab983d24cd2e77ce22ecba47ac
Nicole Prindle <nprindle18@gmail.com>
2021-08-13 04:15:30 -0400

    Remove clever's cache

--- hosts/hecate/nix/caches.nix
+++ hosts/hecate/nix/caches.nix
@@ -5,7 +5,7 @@
     # cache.nixos.org is included by default
     binaryCaches = [
       "https://nprindle.cachix.org"
-      "http://cache.earthtools.ca"
+      # "http://cache.earthtools.ca"
       "https://hydra.iohk.io"
       # haskell-ide-engine cache
       "https://all-hies.cachix.org"
@@ -13,7 +13,7 @@
 
     binaryCachePublicKeys = [
       "nprindle.cachix.org-1:hRW0f/n4hCZZzTzYJO9olDjJ+8MB4VpknEGpiVCxpWo="
-      "c2d.localnet-1:YTVKcy9ZO3tqPNxRqeYEYxSpUH5C8ykZ9ImUKuugf4c="
+      # "c2d.localnet-1:YTVKcy9ZO3tqPNxRqeYEYxSpUH5C8ykZ9ImUKuugf4c="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
     ];
