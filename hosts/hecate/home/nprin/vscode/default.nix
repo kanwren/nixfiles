69ee0ddc253a84cfc1e75bf9c64b77831fa27819
Nicole Prindle <nprindle18@gmail.com>
2021-10-21 14:38:30 -0400

    [hecate] Activate VSCode

--- hosts/hecate/home/nprin/vscode/default.nix
+++ hosts/hecate/home/nprin/vscode/default.nix
@@ -1,15 +1,5 @@
 { pkgs, ... }:
 
 {
-  programs.vscode = {
-    enable = false;
-    package = pkgs.vscodium.overrideAttrs (old: {
-      installPhase = (old.installPhase or "") + ''
-        ln -s "$out"/bin/codium "$out"/bin/code
-      '';
-    });
-    extensions = with pkgs.vscode-extensions; [
-      haskell.haskell
-    ];
-  };
+  programs.vscode.enable = true;
 }
