3195327034ab13c33b47a760afde3702f29ff151
Nicole Prindle <nprindle18@gmail.com>
2021-12-06 23:34:56 -0500

    Move rofi configuration block from theme to nix

--- hosts/hecate/home/nprin/rofi/default.nix
+++ hosts/hecate/home/nprin/rofi/default.nix
@@ -5,6 +5,22 @@
     enable = true;
     terminal = "${pkgs.kitty}/bin/kitty";
     theme = ./theme.rasi;
+    font = "FiraCode Nerd Font 10";
+    extraConfig = {
+      show-icons = true;
+      icon-theme = "hicolor";
+      drun-display-format = "{name}";
+      disable-history = false;
+      fullscreen = false;
+      hide-scrollbar = true;
+      sidebar-mode = false;
+
+      display-ssh = " [ssh]";
+      display-run = " [run]";
+      display-drun = "異 [drun]";
+      display-window = " [window]";
+      display-combi = " [combi]";
+    };
   };
 }
 
