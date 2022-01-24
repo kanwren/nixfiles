09edd28b9aa9e82f430f54abb9e501b991e14f7d
Nicole Prindle <nprindle18@gmail.com>
2022-01-23 20:54:21 -0500

    Add bare i3 config

--- modules/mixins/desktop/x/default.nix
+++ modules/mixins/desktop/x/default.nix
@@ -1,4 +1,5 @@
 {
   base = import ./base;
   i3 = import ./i3;
+  i3-bare = import ./i3-bare;
 }
---
+++ modules/mixins/desktop/x/i3-bare/default.nix
@@ -0,0 +1,28 @@
+{ pkgs, config, ... }:
+
+{
+  services.xserver = {
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+      configFile = pkgs.substituteAll {
+        src = ../i3/i3config/i3config;
+        spill_container_script = pkgs.substituteAll {
+          src = ../i3/i3config/spill_container.sh;
+          isExecutable = true;
+          inherit (pkgs) runtimeShell jq;
+          i3 = config.services.xserver.windowManager.i3.package;
+        };
+      };
+
+      extraPackages = with pkgs; [
+        # Menu/launcher
+        rofi
+        # Status bar
+        i3status
+        # Lock screen
+        i3lock
+      ];
+    };
+  };
+}
