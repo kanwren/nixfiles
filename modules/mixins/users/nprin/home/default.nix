deed5da838e869576c3c58936b7440572966c29c
Nicole Prindle <nprindle18@gmail.com>
2022-02-07 02:12:07 -0500

    [nprin] Add bitwarden

--- modules/mixins/users/nprin/home/default.nix
+++ modules/mixins/users/nprin/home/default.nix
@@ -29,49 +29,40 @@
     };
 
     home = {
-      # User-specific packages. If a program needs configuration, then either:
-      # - Enable and configure it via home-manager, if the option is available
-      #   and configuration can be done entirely in home-manager
-      # - Write the configuration file and use home-manager to manage it
       packages = with pkgs; [
-        # CLI navigation
+        # CLI/TUI stuff
         h # quick directory jumping
-
-        # files, formats
         tokei # count lines of code
         shellcheck # check shell scripts
-
-        # http
         httpie # command line HTTP client
         woof # serve files locally over http
         jo # json generation
+        gotop # activity monitor
 
-        # CLI/TUI clients over other tools
+        # clients
         gitAndTools.gh
         gpg-tui
         ngrok
 
         # development
-        # Rust
         rustup
-        # misc
         tectonic
         graphviz
 
-        # system management, monitoring, and utilities
-        gotop
-        filelight
-        xfce.thunar
-        self.packages.${system}.globus-connect
-
         # applications
+        xfce.thunar # file browser
+        bitwarden # password manager
+        filelight # disk usage visualization
         libreoffice
         discord
         slack
         teams
         spotify
         insomnia # graphical REST API client
-        anki # flashcard app
+        anki # flashcards
+
+        # misc
+        self.packages.${system}.globus-connect
       ];
     };
   };
