5cdf94125067e886ea8eb42a4bd93b3f501bde3e
Nicole Wren <wrenn@squareup.com>
2023-04-06 18:32:10 -0400

    [caspar] Move gitconfig to nix

--- hosts/caspar/configuration.nix
+++ hosts/caspar/configuration.nix
@@ -144,6 +144,91 @@
       enable = true;
     };
 
+    programs.git = {
+      enable = true;
+      userName = "Nicole Wren";
+      userEmail = "wrenn@squareup.com";
+      signing = {
+        signByDefault = true;
+        key = "DCC3076C9F46DFD330C3DFFDA4B4CC3C080B1C66";
+      };
+      includes = [
+        { path = "~/.gitconfig.local"; }
+      ];
+      aliases = {
+        s = "status";
+        cane = "commit --amend --no-edit";
+        amend = "commit --amend";
+        pf = "push --force-with-lease";
+        diffc = "diff --cached";
+        conflicts = "diff --name-status --diff-filter=U";
+        difff = "diff --diff-filter";
+        diffno = "diff --name-only"; # diff file names only; for example, "git diffno --diff-filter=U | xargs vim"
+        diffnof = "diff --name-only --diff-filter";
+        rh = "reset --hard";
+        ri = "rebase --interactive";
+        ls = "log --oneline";
+        lg = "log --graph --abbrev-commit --date=relative --pretty=format:'%C(bold blue)%h - %C(reset)%C(green)(%ar)%C(reset) - %s %C(dim)- %an%C(reset)%C(yellow)%d'";
+        graph = "log --graph --oneline";
+        lr = "log --left-right --graph --oneline";
+        changed = "show --name-status --oneline";
+        mkexec = "update-index --chmod=+x";
+        root = "rev-parse --show-toplevel";
+        ignored = "ls-files --others --exclude-standard";
+
+        alias = ''! f(){ git config --get-regexp ^alias | cut -c 7- | sed -e "s/ \(.*\)/ = \1/"; }; f'';
+        ignore = ''! f(){ curl -sL https://www.toptal.com/developers/gitignore/api/$@ ; }; f'';
+      };
+      extraConfig = {
+        gist.private = true;
+        color = {
+          diff = "auto";
+          status = "auto";
+          branch = "auto";
+          interactive = "auto";
+        };
+        log.mailmap = true;
+        init.defaultBranch = "main";
+        branch.autosetupmerge = true;
+        filter.lfs = {
+          clean = "git-lfs clean -- %f";
+          smudge = "git-lfs smudge -- %f";
+          process = "git-lfs filter-process";
+          required = true;
+        };
+        rerere.enabled = 1;
+        pull.ff = "only";
+        push.default = "simple";
+        diff = {
+          renames = true;
+          indentHeuristic = "on";
+        };
+        rebase = {
+          autosquash = true;
+          autostash = true;
+        };
+        merge = {
+          summary = true;
+          conflictstyle = "diff3";
+        };
+        mergetool = {
+          prompt = false;
+          keepBackup = false;
+        };
+      };
+      ignores = [
+        "*.iml"
+        "*.swp"
+        "*.swo"
+        ".bundle"
+        ".DS_Store"
+        ".idea"
+        ".rbx"
+        "node_modules"
+        "/tags"
+      ];
+    };
+
     # TODO(wrenn): this gets put into `~/.config/k9s`, but I think k9s expects
     # it in `~/Library/Application Support/k9s`.
     xdg.configFile."k9s/skin.yml".source = "${self.packages.${pkgs.system}.catppuccin-k9s}/mocha.yml";
