2891cab294c64cb2e9564d3e91223f2610c48ab9
Nicole Wren <nicole@wren.systems>
2023-09-24 23:40:50 -0400

    Use nixpkgs-unstable in devshell template

--- templates/devshell/flake.nix
+++ templates/devshell/flake.nix
@@ -2,7 +2,7 @@
   description = "Template for a flake with a devShell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
