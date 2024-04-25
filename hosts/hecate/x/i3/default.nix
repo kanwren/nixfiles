4182993983b43c291987834333b358d463b98ba0
Nicole Wren <nicole@wren.systems>
2024-04-25 01:20:02 -0700

    [hecate] use catppuccin colors in i3bar

--- hosts/hecate/x/i3/default.nix
+++ hosts/hecate/x/i3/default.nix
@@ -20,17 +20,29 @@
 
       configFile = pkgs.substituteAll {
         src = ./i3config/i3config;
+
         spill_container_script = pkgs.substituteAll {
           src = ./i3config/spill_container.sh;
           isExecutable = true;
           inherit (pkgs) runtimeShell jq;
           i3 = config.services.xserver.windowManager.i3.package;
         };
+
         pick_game_script = pkgs.substituteAll {
           src = ./i3config/pick_game.sh;
           isExecutable = true;
           inherit (pkgs) rofi;
         };
+
+        i3status_config = pkgs.writeText "i3status.conf" ''
+          ${builtins.readFile "${pkgs.i3status}/etc/i3status.conf"}
+
+          general {
+            color_good = "#a6e3a1"
+            color_degraded = "#f9e2af"
+            color_bad = "#f38ba8"
+          }
+        '';
       };
 
       extraPackages = with pkgs; [
