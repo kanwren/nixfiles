6e08ba9ae4b3da509d06b34b5cf9c50c6c25f6d9
Nicole Wren <wrenn@squareup.com>
2024-05-22 10:11:06 -0700

    [caspar] Fix GOPATH being relative to home

--- hosts/caspar/configuration.nix
+++ hosts/caspar/configuration.nix
@@ -313,7 +313,7 @@ in
       programs.go = {
         enable = true;
         package = pkgs.go_1_22;
-        goPath = "$HOME/Development/go";
+        goPath = "Development/go";
       };
 
       programs.kitty = {
