b02511a81966b50ef018d8986da72dee8a7d2d0a
Nicole Wren <nicole@wren.systems>
2024-07-04 20:21:06 -0700

    [hecate] Use Lix's binary cache

--- hosts/hecate/nix/caches.nix
+++ hosts/hecate/nix/caches.nix
@@ -1,7 +1,13 @@
 {
   nix.settings = {
     # cache.nixos.org is included by default
-    substituters = [ "https://kanwren.cachix.org" ];
-    trusted-public-keys = [ "kanwren.cachix.org-1:uMS7ZtVOdof/PU46BAyehmNDD/P6qCGhYEvYP7X8YfE=" ];
+    substituters = [
+      "https://kanwren.cachix.org"
+      "https://cache.lix.systems"
+    ];
+    trusted-public-keys = [
+      "kanwren.cachix.org-1:uMS7ZtVOdof/PU46BAyehmNDD/P6qCGhYEvYP7X8YfE="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+    ];
   };
 }
