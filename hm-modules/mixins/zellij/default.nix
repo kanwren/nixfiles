f0bf2364817a2c1070fede73ed4f9ed092353c90
Nicole Wren <nicole@wren.systems>
2024-12-13 13:45:45 -0800

    zellij: add initial config

--- hm-modules/mixins/zellij/default.nix
+++ hm-modules/mixins/zellij/default.nix
@@ -12,6 +12,15 @@ in
 
   config = lib.mkIf cfg.enable {
     programs.zellij.enable = true;
+
+    # NOTE: the home-manager toKDL generator represents node children as an
+    # attrset, which is the wrong type for KDL's model (can't repeat keys, and
+    # children are supposed to be ordered). This makes it impossible to use
+    # toKDL to generate repeated children with the same node name, as is needed
+    # here for keybindings.
+    xdg.configFile."zellij/config.kdl" = lib.mkForce {
+      source = ./config.kdl;
+    };
   };
 }
 
