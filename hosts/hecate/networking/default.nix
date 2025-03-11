7098b36037fa1dc20602d5521c8beac97c81a6b3
Nicole Wren <nicole@wren.systems>
2025-03-11 11:41:38 -0700

    [hecate] Simplify networking config

--- hosts/hecate/networking/default.nix
+++ hosts/hecate/networking/default.nix
@@ -4,17 +4,11 @@
       enable = true;
       wifi.backend = "iwd";
     };
-    wireless.enable = false;
 
     firewall = {
       enable = true;
       allowedTCPPorts = [ ];
       allowedUDPPorts = [ ];
     };
-
-    nameservers = [
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
   };
 }
