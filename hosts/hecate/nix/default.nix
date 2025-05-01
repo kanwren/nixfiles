a0b833267eaa656727d139dd65722fc7a954e95b
Nicole Wren <nicole@wren.systems>
2025-05-01 01:22:12 -0700

    fix(hecate): disable automatic GC

--- hosts/hecate/nix/default.nix
+++ hosts/hecate/nix/default.nix
@@ -12,12 +12,6 @@
       keep-derivations = true;
       trusted-users = [ "root" ];
     };
-
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
   };
 
   nixpkgs.config.allowUnfree = true;
