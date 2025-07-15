68dd7333d902157004890675fbe97bf9f3f103ca
Nicole Wren <nicole@wren.systems>
2025-07-14 22:31:16 -0700

    style(birdbox): format

--- hosts/birdbox/hardware-configuration.nix
+++ hosts/birdbox/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
