209a6b079f8600a6b1e7ef4251ac691fc4100cac
Nicole Wren <nicole@wren.systems>
2025-07-28 01:50:28 -0700

    fix(birdbox/ollama): enable nvidia-container-toolkit to fix CUDA acceleration

--- hosts/birdbox/configuration.nix
+++ hosts/birdbox/configuration.nix
@@ -44,13 +44,16 @@
     };
   };
 
-  hardware.nvidia = {
-    prime = {
-      offload.enable = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+  hardware = {
+    nvidia = {
+      prime = {
+        offload.enable = true;
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+      open = false;
     };
-    open = false;
+    nvidia-container-toolkit.enable = true;
   };
 
   networking = {
