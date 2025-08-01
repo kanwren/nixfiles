ffb10dd86e7796e4346579f7d8514f26bd6e22e6
Nicole Wren <nicole@wren.systems>
2025-07-31 21:08:28 -0700

    fix(gh): disable home-manager config management

--- hm-modules/mixins/gh/default.nix
+++ hm-modules/mixins/gh/default.nix
@@ -13,21 +13,8 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.gh = {
-      enable = true;
-
-      extensions = [
-        pkgs.gh-copilot
-        pkgs.gh-ost
-      ];
-
-      settings = {
-        git_protocol = "https";
-        prompt = "enabled";
-        aliases = {
-          co = "pr checkout";
-        };
-      };
-    };
+    home.packages = [
+      pkgs.gitAndTools.gh
+    ];
   };
 }
