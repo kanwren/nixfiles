c08bb0e7d20530c5557317bc93b06c50a06197e7
Nicole Wren <nicole@wren.systems>
2026-05-21 13:13:57 -0700

    feat(hecate): configure ghostty keybindings

--- hosts/hecate/configuration.nix
+++ hosts/hecate/configuration.nix
@@ -401,6 +401,7 @@
         ghostty = {
           enable = true;
           systemd.enable = true;
+          clearDefaultKeybinds = true; # prefer to use the window manager to splits/tabs/etc.
           settings = {
             font-size = 11.0;
             font-family = "FiraCode Nerd Font Mono";
@@ -408,9 +409,31 @@
               "cursor"
               "sudo"
               "title"
-              "no-ssh-env"
+              "ssh-env"
               "ssh-terminfo"
               "path"
+            ];
+            keybind = [
+              "ctrl+shift+p=toggle_command_palette"
+              "ctrl+shift+n=new_window"
+
+              "ctrl+==increase_font_size:1"
+              "ctrl++=increase_font_size:1"
+              "ctrl+-=decrease_font_size:1"
+              "ctrl+0=reset_font_size"
+
+              "ctrl+shift+f=start_search"
+              "escape=end_search"
+              "ctrl+shift+c=copy_to_clipboard:mixed"
+              "ctrl+shift+v=paste_from_clipboard"
+              "ctrl+shift+j=write_screen_file:paste,plain"
+
+              "shift+page_up=scroll_page_up"
+              "shift+page_down=scroll_page_down"
+              "shift+home=scroll_to_top"
+              "shift+end=scroll_to_bottom"
+              "ctrl+shift+page_up=jump_to_prompt:-1"
+              "ctrl+shift+page_down=jump_to_prompt:1"
             ];
           };
         };
