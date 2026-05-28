b7df1b81bab711fb03636e185258d4e353458c05
Nicole Wren <wrenn@block.xyz>
2026-05-28 16:11:08 -0700

    feat(nightjar): switch to ghostty

--- modules/users/wrenn/terminal.nix
+++ modules/users/wrenn/terminal.nix
@@ -1,16 +1,5 @@
 {
   flake.modules.darwin."users/wrenn" = {
-    homebrew.casks = [ "kitty" ];
+    homebrew.casks = [ "ghostty" ];
   };
-
-  flake.modules.homeManager."users/wrenn" =
-    { pkgs, ... }:
-    {
-      programs.kitty = {
-        enable = true;
-        package = pkgs.kitty.overrideAttrs { doInstallCheck = false; };
-        extraConfig = builtins.readFile ./kitty.conf;
-        shellIntegration.mode = "enabled";
-      };
-    };
 }
